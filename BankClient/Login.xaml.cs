0dcfcb0af4b6914d01daf5dfc03daef8789a6052
seby1621199 <sysoru16@gmail.com>
2022-06-16 20:10:30 +0300

    Add Login+Register

--- BankClient/Login.xaml.cs
+++ BankClient/Login.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 
@@ -25,47 +13,103 @@ namespace BankClient
     /// </summary>
     public partial class Login : Window
     {
-        //MongoDB.Driver.MongoClient m_Client;
-        //MongoDB.Driver.IMongoDatabase m_Database;
-        //MongoDB.Driver.IMongoCollection<User> m_Collection;
-        public void  DB()
+        private void DB()
         {
             Globals.m_Client = new MongoClient("mongodb+srv://bank:Drept1234!@cluster0.zwmtb.mongodb.net/Cluster0?retryWrites=true&w=majority");
             Globals.m_Database = Globals.m_Client.GetDatabase("bank");
             Globals.m_Collection = Globals.m_Database.GetCollection<User>("bank");
-            //Globals.test = "ss";
 
         }
 
 
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
+            /* Conectarea la  */
             DB();
-            User user = new User();
-            user.username = user_input.Text;
-            user.balance = 0;
-            user.password= Encryption.Encrypt(password_input.Text); ;
-            //user_input.Text = Globals.test;
-            Globals.m_Collection.InsertOne(user);
-            var password=Encryption.Encrypt(password_input.Text);
-
-            // var t = Globals.m_Collection.Find(filter).FirstOrDefault();
-            // var doc = bank.Find(filter).FirstOrDefault();
-            var filter = Builders<User>.Filter.Eq("username", "saluuut");
-            User user1 = Globals.m_Collection.Find(filter).FirstOrDefault();
-
-
-
-
+            /* */
+            User user = new User
+            {
+                Username = user_input.Text,
+                Balance = 0,
+                Password = Encryption.Encrypt(password_input.Text)
+            };
+            ;
+            //Globals.m_Collection.InsertOne(user);
+            //var password = Encryption.Encrypt(password_input.Text);
+            var filter = Builders<User>.Filter.Eq("Username", user.Username);
+            User search = Globals.m_Collection.Find(filter).FirstOrDefault();
+            //    result_text.Visibility = Visibility.Visible;
+            //   result_text.Content = "User " + user.username + " already exist!";
 
 
+            //if (search!=null)
+            //{
+            //    result_text.Visibility = Visibility.Visible;
+            //    result_text.Content = "User "+search.password+" already exist!";
+            //}
+            if (search != null)
+
+                    if (search.Password == user.Password)
+                    {
+                        result_text.Visibility = Visibility.Visible;
+                        result_text.Content = "Login succesful.";
+                    }
+                    else
+                    {
+                        result_text.Visibility = Visibility.Visible;
+                        result_text.Content = "Wrong password.";
+                    }
+                else
+                {
+                    {
+                        result_text.Visibility = Visibility.Visible;
+                        result_text.Content = "User dont exist.";
+                    }
+                }
+
+
+        }
+
+        private void final_step_register(&User user)
+        {
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
+
+        }
+
+        private void Register_button_Click(object sender, RoutedEventArgs e)
+        {
+            /* Conectarea la  */
+            DB();
+            /* */
+            User user = new User
+            {
+                Username = user_input.Text,
+                Balance = 0,
+                Password = Encryption.Encrypt(password_input.Text)
+            };
+            ;
+
+            var filter = Builders<User>.Filter.Eq("Username", user.Username);
+            User search = Globals.m_Collection.Find(filter).FirstOrDefault();
+
+            if (search == null)
+            {
+                result_text.Visibility = Visibility.Visible;
+                result_text.Content = "Register succesful.";
+                Globals.m_Collection.InsertOne(user);
+            }
+            else
+            {
+                result_text.Visibility = Visibility.Visible;
+                result_text.Content = "User already exist.";
+
+            }
 
         }
     }
